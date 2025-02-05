4836329e9925b87e5048f81936f579cd236b5e81
Jacob Leonardo <leonardojacob257@gmail.com>
2025-02-05 09:35:25 -0700

    Ball base

--- Assets/Scripts/BallBase.cs
+++ Assets/Scripts/BallBase.cs
@@ -2,5 +2,7 @@ using UnityEngine;
 
 public class BallBase : MonoBehaviour
 {
-	
+	[SerializeField] private Rigidbody2D rb;
+	[SerializeField] private Collider2D col;
+	[SerializeField] private SpriteRenderer spriteRenderer;
 }
