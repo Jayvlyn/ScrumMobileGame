8348bf1aa95b7cab386419704dd03573ef800bfa
Seth Kimberlin <sethkimberlin04@gmail.com>
2025-02-11 17:13:02 -0700

    Fixed Powerups

--- Assets/Scripts/BallBase.cs
+++ Assets/Scripts/BallBase.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class BallBase : MonoBehaviour
@@ -6,6 +7,7 @@ public class BallBase : MonoBehaviour
 	[SerializeField] private Collider2D col;
 	[SerializeField] private SpriteRenderer spriteRenderer;
 
+<<<<<<< Updated upstream
 	[Header("Enemy Stuff")]
 	[SerializeField] LayerMask enemyLayer;
 	[SerializeField] float damage;
@@ -27,4 +29,53 @@ public class BallBase : MonoBehaviour
 	{
 		GetComponent<Transform>().localScale += new Vector3( size, size, size );
 	}
+=======
+    private Vector3 originalScale = new Vector3(0.5f, 0.5f, 0.5f);
+    private float sizeResetTimer = 0;
+    private Coroutine sizeReset;
+
+
+
+    public void OnBallSplit()
+    {
+        GameObject ballGameObject = GameManager.instance.ballPool.ActivateObject();
+
+        BallBase ball = ballGameObject.GetComponent<BallBase>();
+
+        ball.transform.position = transform.position + new Vector3(Random.Range(-0.5f, 0.5f), Random.Range(-0.5f, 0.5f), 0);
+
+        float scale = transform.localScale.magnitude - originalScale.magnitude;
+        ball.ChangeScale(scale, sizeResetTimer);
+        ball.rb.linearVelocity = rb.linearVelocity;
+    }
+
+    public void ChangeScale(float newSize, float duration)
+    {
+        if (sizeReset == null)
+        {
+            transform.localScale += new Vector3(newSize, newSize, newSize);
+        }
+        else
+        {
+            StopCoroutine(sizeReset);
+        }
+        sizeReset = StartCoroutine(ResetScaleAfterTime(duration));
+    }
+
+    private IEnumerator ResetScaleAfterTime(float duration)
+    {
+        sizeResetTimer = duration;
+        while (sizeResetTimer > 0)
+        {
+            sizeResetTimer -= Time.deltaTime;
+            yield return null;
+        }
+        ResetScale();
+    }
+
+    private void ResetScale()
+    {
+        transform.localScale = originalScale;
+    }
+>>>>>>> Stashed changes
 }
--- Assets/Scripts/Powerups/BallSplitPowerup.cs
+++ Assets/Scripts/Powerups/BallSplitPowerup.cs
@@ -1,17 +1,13 @@
-using GameEvents;
 using UnityEngine;
 
-public class BallSizePowerup : MonoBehaviour
+public class BallSplitPowerup : MonoBehaviour
 {
-    [SerializeField] float sizeToIncrease = 1.0f;
-    [SerializeField] float time = 5f;
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
         BallBase ball = collision.GetComponent<BallBase>();
         if (ball)
         {
-            ball.ChangeScale(sizeToIncrease, time);
+            ball.OnBallSplit();
             Destroy(gameObject);
         }
     }
