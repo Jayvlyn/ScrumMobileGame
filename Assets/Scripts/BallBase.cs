9e00cd9433b32152a7ad29a9a7c3a2f26b51acd9
Seth Kimberlin <sethkimberlin04@gmail.com>
2025-02-11 20:15:21 -0700

    Fixed Size Change Bug

--- Assets/Scripts/BallBase.cs
+++ Assets/Scripts/BallBase.cs
@@ -14,6 +14,7 @@ public class BallBase : MonoBehaviour
     public Vector3 originalScale = new Vector3(0.3f, 0.3f, 0.3f);
     private float sizeResetTimer = 0;
     private Coroutine sizeReset;
+    private bool inPowerUp = false;
 
 	private void Start()
 	{
@@ -36,18 +37,19 @@ public class BallBase : MonoBehaviour
 
         ball.transform.position = transform.position + new Vector3(Random.Range(-0.5f, 0.5f), Random.Range(-0.5f, 0.5f), 0);
 
-        float scale = transform.localScale.magnitude - originalScale.magnitude;
+        float scale = transform.localScale.magnitude / originalScale.magnitude;
         ball.ChangeScale(scale, sizeResetTimer);
         ball.rb.linearVelocity = rb.linearVelocity;
     }
 
     public void ChangeScale(float newSize, float duration)
     {
-        if (sizeReset == null)
+        if (!inPowerUp)
         {
+            inPowerUp = true;
             transform.localScale *= newSize;
         }
-        else
+        else if (sizeReset != null)
         {
             StopCoroutine(sizeReset);
         }
@@ -67,6 +69,7 @@ public class BallBase : MonoBehaviour
 
     private void ResetScale()
     {
+        inPowerUp = false;
         transform.localScale = originalScale;
     }
 
