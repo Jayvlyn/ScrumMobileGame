609509eb908a5ae0a506ddafe61edfad26730a41
CadenLS <csinclair@student.neumont.edu>
2025-02-18 09:48:46 -0700

    BAsic Setting

---
+++ Assets/Scripts/UI/OptionsPanel.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using System.Collections;
+
+public class OptionsPanel : MonoBehaviour
+{
+    public GameObject uiPanel;
+    public Animator uiAnimator;
+    private bool isOpen = false;
+
+    public void ToggleUI()
+    {
+        if (isOpen)
+        {
+            uiAnimator.Play("OptionsClose");
+            StartCoroutine(DisablePanelAfterAnimation());
+        }
+        else
+        {
+            uiPanel.SetActive(true);
+            uiAnimator.Play("OptionsOpen");
+        }
+
+        isOpen = !isOpen;
+    }
+
+    private IEnumerator DisablePanelAfterAnimation()
+    {
+        yield return new WaitForSeconds(uiAnimator.GetCurrentAnimatorStateInfo(0).length);
+        uiPanel.SetActive(false);
+    }
+}
