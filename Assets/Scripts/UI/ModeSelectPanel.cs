b0f829c82f8edd0739de7da3d58e16a0ba346914
Jacob Leonardo <leonardojacob257@gmail.com>
2025-02-25 00:00:26 -0700

    Main Level Implementation

---
+++ Assets/Scripts/UI/ModeSelectPanel.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using System.Collections;
+
+public class ModeSelectPanel : MonoBehaviour
+{
+    public GameObject uiPanel;
+    public Animator uiAnimator;
+    private bool isOpen = false;
+    [SerializeField] public string openAnim;
+    [SerializeField] public string closeAnim;
+
+	public void ToggleUI()
+    {
+        if (isOpen)
+        {
+            uiAnimator.Play(closeAnim);
+            StartCoroutine(DisablePanelAfterAnimation());
+        }
+        else
+        {
+            uiPanel.SetActive(true);
+            uiAnimator.Play(openAnim);
+        }
+
+        isOpen = !isOpen;
+    }
+
+    private IEnumerator DisablePanelAfterAnimation()
+    {
+        yield return new WaitForSeconds(uiAnimator.GetCurrentAnimatorStateInfo(0).length);
+        uiPanel.SetActive(false);
+    }
+}
